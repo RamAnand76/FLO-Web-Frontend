1ac7b86c98c3627beede17afff75e5a1b82dddce
Ramanand <ramuttenravitsr2002@gmail.com>
2025-10-31 18:47:49 +0530

    fix: Fixed build issue in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,10 @@
 export default function NotFoundPage() {
-    return (
-      <div className="min-h-screen flex items-center justify-center text-center">
-        <div>
-          <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-          <p className="text-gray-600">Sorry, the page you're looking for doesn't exist.</p>
-        </div>
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center">
+      <div>
+        <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="text-gray-600">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
